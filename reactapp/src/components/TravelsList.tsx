dc61725f2872894bfda14c30ededa40942e2b72b
Роман Мишин <mishin_roman@outlook.com>
2023-09-11 21:11:12 +0300

    Доработан лодер анинмация

--- reactapp/src/components/TravelsList.tsx
+++ reactapp/src/components/TravelsList.tsx
@@ -3,17 +3,18 @@ import React, { useState } from "react";
 import travelsService from "../services/travels.service";
 import { ITravel } from "../interfaces/app.interfaces";
 import TravelsCard from "./TravelsCard";
+import Loading from "./UI/loading";
+
 
 type Props = {};
-  
+
 const TravelsList = (props: Props) => {
-  
   const { isLoading, isError, data, error } = useQuery(
     ["travels"],
     () => travelsService.GetTravels(),
     {
       select: ({ data }) => data,
-      
+
       onError(err) {
         return err;
       },
@@ -22,18 +23,20 @@ const TravelsList = (props: Props) => {
       //   cacheTime:10,//время х
     }
   );
- 
+
   return isError ? (
     <div>{(error as Error).message}</div>
   ) : (
     <div className="container flex pt-4 justify-center flex-wrap">
       {isLoading ? (
-        <div>Loding...</div>
-        ) : (
+        <div>
+         <Loading/>
+          loading...
+        </div>
+      ) : (
         data?.map((d) => <TravelsCard key={d.id} travel={d} />)
       )}
     </div>
-   
   );
 };
 
